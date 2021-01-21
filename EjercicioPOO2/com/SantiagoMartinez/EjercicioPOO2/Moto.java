d686cd3a2f6f1369ea771b72da13c3ce39f00270
Santiago Martinez <64985640+SJMartinez@users.noreply.github.com>
2021-01-21 14:47:13 -0300

    Add files via upload
    segundo commit

---
+++ EjercicioPOO2/com/SantiagoMartinez/EjercicioPOO2/Moto.java
@@ -0,0 +1,17 @@
+package com.SantiagoMartinez.EjercicioPOO2;
+
+public class Moto extends Vehiculo  {
+
+	private String cilindrada;
+	
+	public Moto (String marca, String modelo, String precio,String cilindrada) {
+		super(marca, modelo, precio);
+		this.cilindrada = cilindrada;
+	}
+	
+	@Override
+	public void imprimir() {
+		System.out.println("Marca: " + this.marca + " // Modelo: " + this.modelo + " // Cilindrada: " + this.cilindrada + " // Precio: $" + this.precio);
+	}
+	
+}
